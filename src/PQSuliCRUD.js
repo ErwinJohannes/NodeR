e56f41c23af5abcf4d426f15f522be7197dd5729
Vincent Fabron <152129153+ErwinJohannes@users.noreply.github.com>
2024-02-22 14:11:27 +0700

    bb

--- src/PQSuliCRUD.js
+++ src/PQSuliCRUD.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(expess.json());
 
-const dbUrl = ' https://node58996-ball-comeback.proen.app.ruk-com.cloud'
+const dbUrl = 'postgres://webadmin:MAIgsf81141@node58996-ball-comeback.proen.app.ruk-com.cloud:11882/Books'
 
 const sequelize  = new Sequelize(dbUrl);
